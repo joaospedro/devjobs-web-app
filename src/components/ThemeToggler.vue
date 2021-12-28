b446128af31502452eba5d024fa4c539fc36be49
joaospedro <joaospedro2516@gmail.com>
2021-12-28 18:39:30 -0300

    add conditional validation to theme toggler

--- src/components/ThemeToggler.vue
+++ src/components/ThemeToggler.vue
@@ -2,7 +2,8 @@
   <div class="theme-toggler flex items-center space-x-1.5">
   <img :src="iconSun" />
   <div @click="toggleTheme" class="w-12 h-6 rounded-xl bg-white cursor-pointer p-1.5">
-    <div class="round transform translate-x-6 transition-transform duration-700 w-3 h-3 bg-violet rounded-3xl" />
+    <div v-if="this.$store.state.theme.theme == 'dark'" class="transform translate-x-6 w-3 h-3 bg-violet rounded-3xl" />
+    <div v-else-if="this.$store.state.theme.theme == 'light'" class="w-3 h-3 bg-violet rounded-3xl" />
     </div>
     <img class="pl-0.5" :src="iconMoon" />
   </div>
@@ -20,9 +21,6 @@ export default {
   methods: {
     toggleTheme() {
       this.$store.dispatch("toggleTheme");
-
-      const round = document.querySelector(".round")
-      round.classList.toggle("translate-x-6");
     },
   },
 };
